cdb31e1753dd66a10c31b8d57d9194508b71581d
Urfan Guliyev <urfan196@yahoo.com>
2019-09-23 02:42:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,4 +10,17 @@ attr_accessor :name
     Post.all.select {|post| post.author == self}
   end
 
+  def add_post (post)
+  post.author = self
+  end
+
+  def add_post_by_title (title)
+    post = Post.new(title)
+    add_post(post)
+  end
+
+  def post_count
+    Post.all.count
+  end
+
 end
