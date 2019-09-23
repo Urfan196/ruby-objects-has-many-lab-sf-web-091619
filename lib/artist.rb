4494d0c692e9aaa49a54123e017a9b474e88522b
Urfan Guliyev <urfan196@yahoo.com>
2019-09-23 02:06:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+
+attr_accessor :name
+
+  def initialize (name)
+  @name = name
+  end
+
+  def add_song (song)
+    song.artist = self
+  end
+
+  def songs
+  Song.all? { |song| song.artist == self}
+  end
+
+  # def add_song (song)
+  #   @songs << Song.new(song)
+  # end
+  #
+  # def songs
+  #   @songs
+  # end
+  #
+  # def add_song_by_name
+  #
+
+
+
+end
